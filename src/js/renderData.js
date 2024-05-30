e033a0d7fd717594ad9c04141c6310d0c959dc86
Hannah Høilo <143997275+Hannahhoilo@users.noreply.github.com>
2024-05-30 15:00:13 +0200

    removed uneccecary code and added a readme file

--- src/js/renderData.js
+++ src/js/renderData.js
@@ -1,78 +1,8 @@
 
-/*
-
-
-
-
-
-------------------------------------------------------
-
-
-const gameList = document.querySelector(".game-list");
-
-const url = 'https://api.rawg.io/api/games?key=3c463ef7d0934f34bd20df5f0297ed5f'
-
-function videoGames(url){
-	fetch(url)
-	.then(response => response.json())
-	.then(data => {
-		console.log(data)
-	})
-}
-
-videoGames(url);
-
-
-
-
-function renderData(videoGames){
-	const mainContentContainer = document.querySelector('.main-content-container');
-
-	videoGames.forEach((videoGame)=>{
-
-		let newVideoGame = videoGame.data();
-
-		const videoGameContainer = document.createElement('span');
-		const gameContainer = document.createElement('span');
-		const gameImage = document.createElement('img');
-		const videoGameName = document.createElement('h3');
-		const countryRegion = document.createElement('h3');
-		const countryPopulation = document.createElement('h3');
-
-		const videoGameReleased = document.createElement("p");
-		const videoGamePlatforms = document.createElement("p");
-		const videoGameDevelopers = document.createElement("p");
-
-		
-
-		mainContentContainer.append(videoGameContainer);
-		videoGameContainer.append(gameContainer, videoGameName, countryRegion, countryPopulation, videoGameReleased, videoGamePlatforms, videoGameDevelopers);
-		gameContainer.append(gameImage);
-
-		gameList.append(li);
-		li.append(name, birthYear, species, homeworld, films, imgContainer);
-
-		gameImage.src = newVideoGame.game;
-		videoGameName.textConent = newVideoGame.name;
-		countryRegion.textContent = newCountry.region;
-		countryPopulation.textContent = newCountry.population;
-
-		videoGameContainer.textContent = newVideoGame.videoGameContainer;
-		gameContainer.textContent = newVideoGame.gameContainer;
-		videoGameReleased.textContent = newVideoGame.videoGameReleased;
-
-	});
-}
- */
-
-/* import {fetchedGameData} from './app'
-
-console.log("from render", fetchedGameData); */
-
  function renderData(videoGames){
     const gameList = document.querySelector('.game-list');
 	gameList.textContent = ''
-console.log(videoGames);
+	console.log(videoGames);
      videoGames.forEach((videoGame) => {
 		
         const gameCard = document.createElement('div');
@@ -117,32 +47,5 @@ console.log(videoGames);
 
     });
 }
-
-
-/* Filter */
-
-/* // Function to filter games by genre
-function filterByGenre(newVideoGame, genres) {
-    return filterVideoGames.filter(game => game.genres === genres);
-}
-
-// Function to filter games by platform
-function filterByPlatform(newVideoGame, platform) {
-    return filterVideoGames.filter(game => game.platform === platform);
-}
-
-// Example of applying filters based on user input
-const filteredByGenre = filterByGenre(games, 'Action');
-console.log('Games filtered by genre:', filteredByGenres);
-
-const filteredByPlatform = filterByPlatform(games, 'PlayStation');
-console.log('Games filtered by platform:', filteredByPlatform);
-
-
- */
-
-
-
-
 
 export default renderData;
